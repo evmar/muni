2afa6c472ade8b43ed35d17cc54b9cb085cf75ce
Evan Martin <martine@danga.com>
2009-04-11 22:14:22 -0700

    sketch of basic UI

--- android/src/org/neugierig/muni.java
+++ android/src/org/neugierig/muni.java
@@ -2,14 +2,23 @@ package org.neugierig;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
 
-public class muni extends Activity
-{
+public class muni extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.stop);
+
+        ListView list = (ListView) findViewById(R.id.list);
+        String[] entries = { "foo", "bar", "x", "x", "y", "z", "g" };
+        ListAdapter adapter = new ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            entries);
+        list.setAdapter(adapter);
     }
 }
