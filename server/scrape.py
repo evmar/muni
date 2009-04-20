6ac012c5899fabaa56677e7a797a315a95beb292
Evan Martin <martine@danga.com>
2009-04-19 20:25:58 -0700

    rename scraper to simple in anticipation of wireless

--- server/scrape.py
+++ server/scrape.py
@@ -3,73 +3,73 @@
 from BeautifulSoup import BeautifulSoup
 import re
 
-def scrape_status(file):
-    soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
-
-    # Skip to the beginning of the interesting content.
-    node = soup.find(text=re.compile(r'Next\s+vehicles\s+for\s+line'))
-
-    times = []
-    extract_time = re.compile(r'(\d+)')
-
-    # Extract all the times from the page.
-    for n in node.findAllNext('div'):
-        text = n.contents[0].string
-        if text is None:
-            break
-        match = extract_time.search(text)
-        if match is None:
-            break
-        times.append(int(match.group(1)))
-
-    return times
-
 class StopTable(object):
     def __init__(self, name=None, stops=None):
         self.name = name
         self.stops = stops
-
-class Stop(object):
-    def __init__(self, name, url):
-        self.name = name
-        self.url = url
-
-def scrape_stops(file):
-    soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
-
-    node = soup.find(text=re.compile(r'Direction:'))
-
-    dir_names = []
-    for direction in node.findAllNext('b'):
-        dir_names.append(direction.string.strip())
-
-    dir_stops = []
-    tables = node.findAllNext('table')
-    for table in tables:
-        stops = []
-        for a in table.findAll('a'):
-            stops.append(Stop(name=a.findNext(text=re.compile(r'\w+')).string,
-                              url=a['href']))
-        dir_stops.append(stops)
-
-    assert len(dir_names) == 2
-    assert len(dir_stops) == 2
-    return (StopTable(dir_names[0], dir_stops[0]),
-            StopTable(dir_names[1], dir_stops[1]))
-
 
 class Route(object):
     def __init__(self, name, url):
         self.name = name
         self.url = url
 
+class Stop(object):
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
 
-def scrape_routes(file):
-    soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
+class Simple:
+    def scrape_status(self, file):
+        soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
 
-    routes = []
-    for route in soup.findAll('a', href=re.compile(r'simpleStopSelector')):
-        url = route.href
-        name = route.findNext(text=re.compile(r'\w+')).string
-        routes.append(Route(name, url))
-    return routes
+        # Skip to the beginning of the interesting content.
+        node = soup.find(text=re.compile(r'Next\s+vehicles\s+for\s+line'))
+
+        times = []
+        extract_time = re.compile(r'(\d+)')
+
+        # Extract all the times from the page.
+        for n in node.findAllNext('div'):
+            text = n.contents[0].string
+            if text is None:
+                break
+            match = extract_time.search(text)
+            if match is None:
+                break
+            times.append(int(match.group(1)))
+
+        return times
+
+    def scrape_stops(self, file):
+        soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
+
+        node = soup.find(text=re.compile(r'Direction:'))
+
+        dir_names = []
+        for direction in node.findAllNext('b'):
+            dir_names.append(direction.string.strip())
+
+        dir_stops = []
+        tables = node.findAllNext('table')
+        for table in tables:
+            stops = []
+            for a in table.findAll('a'):
+                stops.append(Stop(name=a.findNext(text=re.compile(r'\w+')).string,
+                                  url=a['href']))
+            dir_stops.append(stops)
+
+        assert len(dir_names) == 2
+        assert len(dir_stops) == 2
+        return (StopTable(dir_names[0], dir_stops[0]),
+                StopTable(dir_names[1], dir_stops[1]))
+
+
+    def scrape_routes(self, file):
+        soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
+
+        routes = []
+        for route in soup.findAll('a', href=re.compile(r'simpleStopSelector')):
+            url = route.href
+            name = route.findNext(text=re.compile(r'\w+')).string
+            routes.append(Route(name, url))
+        return routes
--- server/scrape_test.py
+++ server/scrape_test.py
@@ -4,20 +4,23 @@ import unittest
 
 import scrape
 
-class TestScrape(unittest.TestCase):
+class TestSimpleScrape(unittest.TestCase):
+    def setUp(self):
+        self.scraper = scrape.Simple()
+
     def testStatus(self):
-        times = scrape.scrape_status(open('data/status.html'))
+        times = self.scraper.scrape_status(open('data-simple/status.html'))
         self.assertEqual([12, 29, 30], times)
 
     def testStops(self):
-        stops = scrape.scrape_stops(open('data/stops.html'))
+        stops = self.scraper.scrape_stops(open('data-simple/stops.html'))
         self.assertEqual(u'Inbound to Embarcadero Station', stops[0].name)
         self.assertEqual(u'Church St & Clipper St', stops[0].stops[10].name)
         self.assertEqual(25, len(stops[0].stops))
         self.assertEqual(23, len(stops[1].stops))
 
     def testRoutes(self):
-        routes = scrape.scrape_routes(open('data/routes.html'))
+        routes = self.scraper.scrape_routes(open('data-simple/routes.html'))
         self.assertEqual(87, len(routes))
         self.assertEqual("J-Church", routes[1].name)
 
