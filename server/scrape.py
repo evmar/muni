3efdaa3b85a1006e3de3bcbd6be9e350ff02583e
Evan Martin <martine@danga.com>
2009-04-19 21:12:33 -0700

    use wireless, probably breaking simple

--- server/scrape.py
+++ server/scrape.py
@@ -8,15 +8,19 @@ class StopTable(object):
         self.name = name
         self.stops = stops
 
-class Route(object):
+class NamedURL(object):
     def __init__(self, name, url):
         self.name = name
         self.url = url
 
-class Stop(object):
-    def __init__(self, name, url):
-        self.name = name
-        self.url = url
+class Route(NamedURL):
+    pass
+
+class Direction(NamedURL):
+    pass
+
+class Stop(NamedURL):
+    pass
 
 class Simple:
     def scrape_status(self, file):
@@ -73,3 +77,51 @@ class Simple:
             name = route.findNext(text=re.compile(r'\w+')).string
             routes.append(Route(name, url))
         return routes
+
+class Wireless:
+    def _scrape_list(self, file, target):
+        soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
+
+        for entry in soup.findAll('a', href=target):
+            url = entry['href']
+            name = entry.findNext(text=re.compile(r'\w+')).string
+            yield (name, url)
+
+    def scrape_routes(self, file):
+        return [Route(name, url) for (name, url)
+                in self._scrape_list(file, re.compile(r'miniDirection.shtml'))]
+
+    def scrape_direction(self, file):
+        return [Direction(name, url) for (name, url)
+                in self._scrape_list(file, re.compile(r'miniStop.shtml'))]
+
+    def scrape_stops(self, file):
+        return [Stop(name, url) for (name, url)
+                in self._scrape_list(file, re.compile(r'miniPrediction.shtml'))]
+
+    def scrape_stop(self, file):
+        soup = BeautifulSoup(file, convertEntities=BeautifulSoup.ALL_ENTITIES)
+
+        times = []
+
+        # Skip to the beginning of the interesting content.
+        node = soup.find(text=re.compile(r'Next vehicles in'))
+
+        if not node:
+            # No next times?
+            return times
+
+        extract_time = re.compile(r'(\d+)')
+
+        # Extract all the times from the page.
+        for n in node.findAllNext('div'):
+            text = n.contents[0].string
+            if text is None:
+                break
+            match = extract_time.search(text)
+            if match is None:
+                break
+            times.append(int(match.group(1)))
+
+        return times
+
--- server/scrape_test.py
+++ server/scrape_test.py
@@ -24,5 +24,34 @@ class TestSimpleScrape(unittest.TestCase):
         self.assertEqual(87, len(routes))
         self.assertEqual("J-Church", routes[1].name)
 
+class TestWirelessScrape(unittest.TestCase):
+    def setUp(self):
+        self.scraper = scrape.Wireless()
+
+    def testRoutes(self):
+        routes = self.scraper.scrape_routes(open('data-wireless/miniRoute.shtml?a=sf-muni'))
+        self.assertEqual(84, len(routes))
+        self.assertEqual("F-Market & Wharves", routes[0].name)
+        self.assertEqual("miniDirection.shtml?a=sf-muni&r=J", routes[1].url)
+        self.assertEqual("California Street Cable Car", routes[-1].name)
+
+    def testRoute(self):
+        dirs = self.scraper.scrape_direction(open('data-wireless/miniDirection.shtml?a=sf-muni&r=J'))
+        self.assertEqual(2, len(dirs))
+        self.assertEqual('Inbound to Embarcadero Station', dirs[0].name)
+        self.assertEqual('miniStop.shtml?a=sf-muni&r=J&d=J__IB2', dirs[0].url)
+
+    def testStops(self):
+        stops = self.scraper.scrape_stops(open('data-wireless/miniStop.shtml?a=sf-muni&r=J&d=J__IB2'))
+        self.assertEqual(25, len(stops))
+
+    def testStopEmpty(self):
+        times = self.scraper.scrape_stop(open('data-wireless/miniPrediction.shtml?a=sf-muni&r=J&d=J__IB2&s=6217'))
+        self.assertEqual(0, len(times))
+
+    def testStop(self):
+        times = self.scraper.scrape_stop(open('data-wireless/miniPrediction.shtml?a=sf-muni&r=N&d=N__IB3&s=5419'))
+        self.assertEqual([1,6,25], times)
+
 if __name__ == '__main__':
     unittest.main()
