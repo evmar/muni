1d318f594342178afac20c486281c0ed353d9582
Evan Martin <martine@danga.com>
2009-05-02 17:39:12 -0700

    split list adapter, and demo on front page

--- android/src/org/neugierig/muni/Routes.java
+++ android/src/org/neugierig/muni/Routes.java
@@ -3,17 +3,27 @@ package org.neugierig.muni;
 import android.app.*;
 import android.content.*;
 import android.os.Bundle;
-import android.widget.*;
+import android.util.Log;
 import android.view.View;
+import android.widget.*;
 
 public class Routes extends ListActivity
                     implements AsyncBackendHelper.Delegate {
   private MuniAPI.Route[] mRoutes;
+  private SplitListAdapter mSplitListAdapter;
   private AsyncBackendHelper mBackendHelper;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    ListAdapter foo = new ArrayAdapter<String>(
+        this,
+        android.R.layout.simple_list_item_1,
+        new String[] { "(Bookmarks will", "go here)" });
+    mSplitListAdapter = new SplitListAdapter(this, "All Routes");
+    mSplitListAdapter.setAdapter1(foo);
+    setListAdapter(mSplitListAdapter);
 
     mBackendHelper = new AsyncBackendHelper(this, this);
     mBackendHelper.start();
@@ -36,15 +46,20 @@ public class Routes extends ListActivity
         this,
         android.R.layout.simple_list_item_1,
         mRoutes);
-    setListAdapter(adapter);
+    mSplitListAdapter.setAdapter2(adapter);
   }
 
   @Override
   protected void onListItemClick(ListView l, View v, int position, long id) {
-    MuniAPI.Route route = mRoutes[position];
-    Intent intent = new Intent(this, Route.class);
-    intent.putExtra(Route.KEY_ROUTE, route.name);
-    intent.putExtra(Backend.KEY_QUERY, route.url);
-    startActivity(intent);
+    if (mSplitListAdapter.isInList1(position)) {
+      // TODO: implement me.
+    } else {
+      MuniAPI.Route route =
+          mRoutes[mSplitListAdapter.translateList2Position(position)];
+      Intent intent = new Intent(this, Route.class);
+      intent.putExtra(Route.KEY_ROUTE, route.name);
+      intent.putExtra(Backend.KEY_QUERY, route.url);
+      startActivity(intent);
+    }
   }
 }
